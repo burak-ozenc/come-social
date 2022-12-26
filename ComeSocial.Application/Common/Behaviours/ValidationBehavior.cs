6a50a9fcf1d73dd583ee3046a1ac442dbd2e6237
burrrrakozenc <brk.ozenc@gmail.com>
2022-12-26 12:31:14 +0300

    FluentValitor added, error handling enriched

---
+++ ComeSocial.Application/Common/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using ComeSocial.Application.Authentication.Commands.Register;
+using ComeSocial.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+ 
+namespace ComeSocial.Application.Common.Behaviours;
+
+// public class ValidationBehaviours()
+// {
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator;
+    // private IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>> _pipelineBehaviorImplementation;
+    public ValidationBehavior(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+    
+    public async Task<TResponse> Handle(
+        TRequest request, 
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult is null)
+            return await next();
+
+        var errors =
+            validationResult.Errors
+                .ConvertAll(validationFailure => Error.Validation(
+                validationFailure.PropertyName,
+                validationFailure.ErrorMessage))
+                .ToList();
+        // after handler
+        return (dynamic)errors;
+        // return await _pipelineBehaviorImplementation.Handle(request, next, cancellationToken);
+    }
+}
+// }
