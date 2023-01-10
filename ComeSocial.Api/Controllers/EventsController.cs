f812b1fc6b6864393adfb477214ab30028144421
burrrrakozenc <brk.ozenc@gmail.com>
2023-01-11 02:47:14 +0300

    documentation started

---
+++ ComeSocial.Api/Controllers/EventsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComeSocial.Api.Controllers;
+
+[Route("events")]
+public class EventsController : ApiController
+{
+    [HttpGet("listEvents")]
+    public IActionResult ListEvents()
+    {
+        return Ok(Array.Empty<string>());
+    }
+}
--- ComeSocial.Infrastructure/DependencyInjection.cs
+++ ComeSocial.Infrastructure/DependencyInjection.cs
@@ -1,25 +1,55 @@
-﻿using ComeSocial.Application.Common.Interfaces.Authentication;
+﻿using System.Text;
+using ComeSocial.Application.Common.Interfaces.Authentication;
 using ComeSocial.Application.Common.Interfaces.Persistence;
 using ComeSocial.Application.Common.Interfaces.Services;
 using ComeSocial.Infrastructure.Authentication;
 using ComeSocial.Infrastructure.Persistence;
 using ComeSocial.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ComeSocial.Infrastructure;
+
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
         ConfigurationManager configuration)
     {
-        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddAuth(configuration);
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
 
         services.AddScoped<IUserRepository, UserRepository>();
         ;
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            });
+
         return services;
     }
 }
