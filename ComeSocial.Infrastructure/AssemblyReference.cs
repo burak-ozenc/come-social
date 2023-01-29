dc8f8ffa466b6eaebf1be3c3ceff48013545fd8a
Burak Ozenc <brk.ozenc@gmail.com>
2023-01-29 08:35:23 +0300

    tests started basic architectural tests added

---
+++ ComeSocial.Infrastructure/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace ComeSocial.Infrastructure;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
